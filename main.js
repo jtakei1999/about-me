2f5cbf50377081a780ec9228638e1d2aa06e74ef
jtakei1999 <joe-take@softbank.ne.jp>
2021-10-22 21:09:29 +0900

    portfolio

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
-const showImage = document.getElementById("show-image");
-const button = document.getElementById("button");
+const quiz1 = document.getElementById("quiz1");
+const quiz2 = document.getElementById("quiz2");
+const quiz3 = document.getElementById("quiz3");
+const quiz4 = document.getElementById("quiz4");
+const answer1 = document.getElementById("answer1");
+const answer2 = document.getElementById("answer2");
+const answer3 = document.getElementById("answer3");
+const answer4 = document.getElementById("answer4");
+const myPhoto = document.getElementById("my-photo");
+const topicText = document.getElementById("topic-text");
 
-window.addEventListener("load", function () {
-  console.log("load：リソースファイルを全て読み込みました。");
-  const image = document.createElement("img");
-  image.src = "./IMG_6119.jpg";
-  image.classList.add("image");
-  image.id = "image";
+let delay = 1000 / 50;
 
-  showImage.append(image);
-});
-
-function clickTest() {
-  target = document.getElementById("image");
-  if (target.className == null || target.className == "") {
-    target.className = "active";
-  } else {
-    target.className = "";
-  }
-}
+quiz1.onclick = function () {
+  answer1.classList.toggle("a-show");
+  quiz1.classList.toggle("open-q-part");
+};
+quiz2.onclick = function () {
+  answer2.classList.toggle("a-show");
+  quiz2.classList.toggle("open-q-part");
+};
+quiz3.onclick = function () {
+  answer3.classList.toggle("a-show");
+  quiz3.classList.toggle("open-q-part");
+};
+quiz4.onclick = function () {
+  answer4.classList.toggle("a-show");
+  quiz4.classList.toggle("open-q-part");
+};
